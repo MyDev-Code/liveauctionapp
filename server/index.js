4dce6fae41e96d3a8fa9cd3dc48f088e47aa8827
Christin <christinvmathewofficial@gmail.com>
2026-02-06 10:52:50 +0530

    Update

--- server/index.js
+++ server/index.js
@@ -72,7 +72,7 @@ io.on('connection', (socket) => {
   socket.on('SYNC_TIME', (cb) => cb(Date.now()));
 });
 
-app.get('*', (req, res) => {
+app.get('(.*)', (req, res) => {
   res.sendFile(path.join(buildPath, 'index.html'));
 });
 
